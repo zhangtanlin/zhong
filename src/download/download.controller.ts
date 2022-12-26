30a0c785b476dcf57bb48cf03451f62a5cc334b9
zhan1.ba0999@gmail.com <zhan1.ba0999@gmail.com>
2022-12-26 17:42:50 +0700

    优化下载控制器

--- src/download/download.controller.ts
+++ src/download/download.controller.ts
@@ -1,10 +1,10 @@
 /**
  * 导入
- * @requires Controller  - nest模块common导出的控制器
- * @requires Post        - nest模块common导出的post请求方式
- * @requires HttpCode    - nest模块common导出的http状态码
- * @requires Body        - nest模块common导出的Body请求参数
- * @requires Headers     - nest模块common导出的Headers请求参数
+ * @requires Controller nest模块common导出的控制器
+ * @requires Post       nest模块common导出的post请求方式
+ * @requires HttpCode   nest模块common导出的http状态码
+ * @requires Body       nest模块common导出的Body请求参数
+ * @requires Headers    nest模块common导出的Headers请求参数
  */
 import {
   HttpException,
@@ -19,12 +19,8 @@ import {
 } from '@nestjs/common'
 import { DownloadService } from './download.service'
 import { DtoPipe } from '../common/pipe/dto.pipe'
-import { ResultDto } from '../common/dto/result.dto'
 import { DownloadGetDto } from './dto/user.get.dto'
-import * as fs from "fs";
-import * as path from "path";
-import { Stream } from 'stream'
-import { resolve } from 'dns'
+import { createReadStream } from "fs";
 
 /**
  * 下载控制器
@@ -32,23 +28,22 @@ import { resolve } from 'dns'
 @Controller('/api/download')
 export class DownloadController {
   /**
-   * @function [constructor]     - 类中定义的构造函数
-   * @param    [private]         - 修饰的属性或方法是私有的，不能在声明它的类的外部访问
-   * @param    [readonly]        - 表示属性或方法只能使用不能修改
-   * @param    [downloadService] - 服务别名
-   * @class    [DownloadService] - 服务
+   * @function [constructor]     类中定义的构造函数
+   * @param    [private]         修饰的属性或方法是私有的，不能在声明它的类的外部访问
+   * @param    [readonly]        表示属性或方法只能使用不能修改
+   * @param    [downloadService] 服务别名
+   * @class    [DownloadService] 服务
    */
   constructor(private readonly downloadService: DownloadService) { }
   /**
    * 请求列表
-   * @param     [Get]        - 请求方式
-   * @param     [UsePipes]   - 管道验证
-   * @class     [DtoPipe]    - 自己写的管道验证器【和DownloadGetDto配合使用】
-   * @param     [HttpCode]   - 请求返回状态码
-   * @faunction [Body]       - nest提供的获取Body参数的方法
-   * @faunction [Query]      - query参数
-   * @faunction [UserGetDto] - 用以验证Body参数正确与否的dto方法
-   * @returns   {ResultDto}  - 返回值是一个含有提示信息的对象
+   * @param     [Get]        请求方式
+   * @param     [UsePipes]   管道验证
+   * @class     [DtoPipe]    自己写的管道验证器【和DownloadGetDto配合使用】
+   * @param     [HttpCode]   请求返回状态码
+   * @faunction [Body]       nest提供的获取Body参数的方法
+   * @faunction [Query]      query参数
+   * @faunction [UserGetDto] 用以验证Body参数正确与否的dto方法
    */
   @Get()
   @UsePipes(DtoPipe)
@@ -58,7 +53,7 @@ export class DownloadController {
     try {
       const list = await this.downloadService.find(querys)
       // 实现文件下载 
-      var stream = fs.createReadStream(list[0].url);
+      var stream = createReadStream(list[0].url);
       res.set({
         'Content-Type': 'application/octet-stream',
         'Content-Disposition': 'attachment; filename=app.apk',
